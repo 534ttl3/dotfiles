11dab14b702ce65b61b25a2d105d2a53a84b0ec6
Chris S <534ttl3@gmail.com>
2018-06-18 10:39:57 +0200

    new snippets

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -2,7 +2,7 @@
 alias ll='ls -l'
 alias la='ls -al'
 alias latexmain='latexmk -pv -pvc -interaction=nonstopmode -pdf -pdflatex=lualatex -bibtex main.tex'
-alias latexmainandglossaries='latexmk -pv -pvc -interaction=nonstopmode -pdf -pdflatex="lualatex %S; makeglossaries main" -bibtex main.tex'
+alias latexmainandglossaries='latexmk -pv -pvc -interaction=nonstopmode -pdf -pdflatex="lualatex %S; makeglossaries main" main.tex'
 alias cddb='cd ~/Dropbox'
 alias origpath='export PATH=/home/chris/opt/vim/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 alias addanaconda='export PATH="/home/chris/opt/anaconda3/bin:$PATH"'
