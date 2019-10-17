d3f789dec03a18e9691df6e253566a532d2fec40
Your Name <you@example.com>
2019-10-17 04:50:28 +0200

    yasnippets bug fixed

--- .emacs.d/snippets/org-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `org-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'org-mode
-		     '(("pl" "[[pdfview:$1::$2][Link]]\n\n" "org-pdfview-link" nil nil nil "/home/chris/.emacs.d/snippets/org-mode/org-pdfview-link" nil nil)))
-
-
-;;; Do not edit! File generated at Fri Mar  8 09:49:12 2019
