04940fd6b88fdaff733a7b6a53c65312bc0bd853
Your Name <you@example.com>
2020-03-11 00:17:11 +0100

    added jumping to definition from within org babel code block

--- .emacs.d/elisp/cs-org-babel.el
+++ .emacs.d/elisp/cs-org-babel.el
@@ -36,5 +36,35 @@
 
 (setq org-confirm-babel-evaluate nil)
 
+
+;; go to definition from org block
+(defun cs-jump-to-def-from-inside-org-code-block ()
+  "From an `org-mode` buffer's code block, jump to definition."
+  (interactive)
+  (let* (original-org-buffer
+         original-org-buffer-point
+         code-block-buffer
+         code-block-buffer-point
+         new-file-buffer
+         new-file-buffer-point)
+    (setq original-org-buffer (current-buffer))
+    (setq original-org-buffer-point (point))
+    (org-edit-special)
+    (setq code-block-buffer (current-buffer))
+    (setq code-block-buffer-point (point))
+    (with-demoted-errors
+        (call-interactively (global-key-binding (kbd "C-, d"))))
+    (if (not (eq code-block-buffer (current-buffer)))
+        (progn
+          (setq new-file-buffer (current-buffer))
+          (setq new-file-buffer-point (point))
+          (switch-to-buffer code-block-buffer)
+          (goto-char code-block-buffer-point)))
+    (org-edit-src-exit)
+    (if new-file-buffer
+        (progn
+          (switch-to-buffer new-file-buffer)
+          (goto-char new-file-buffer-point)))))
+
 (provide 'cs-org-babel)
 ;;; cs-org-babel.el ends here
